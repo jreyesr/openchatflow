f91717e48022af3c2af5e8b49ba54d4cef7e3eac
jreyesr <jreyesr@users.noreply.github.com>
2023-05-21 22:12:02 -0500

    Force list page to not prerender on build

--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -5,6 +5,7 @@ import { prisma } from "@/lib/prisma";
 import Pencil from "@/icons/pencil.svg";
 import Add from "@/icons/add.svg";
 
+export const dynamic = "force-dynamic";
 export default async function DesignList() {
   const flows = await prisma.conversationTemplate.findMany({
     orderBy: { createdAt: "desc" },
